e0e1bb55556626fb4fc41fc386f49030fdd9a7d5
abdularis <abdularisrahmanudin10@gmail.com>
2023-04-05 15:41:28 +0700

    update mod 2

--- mod-super/print.go
+++ mod-super/print.go
@@ -3,5 +3,5 @@ package modsuper
 import "fmt"
 
 func PrintModSuper() {
-	fmt.Println("print from module super (THIS IS VERSION v2.0.1) 🍉")
+	fmt.Println("print from module super (THIS IS VERSION 1 something) 🍉")
 }
