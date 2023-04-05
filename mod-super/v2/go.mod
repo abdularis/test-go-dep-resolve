09dba8ae6b68e57dbb69a19c392a3f57c88a583f
abdularis <abdularisrahmanudin10@gmail.com>
2023-04-05 15:32:09 +0700

    update mod 2

--- mod-super/v2/go.mod
+++ mod-super/v2/go.mod
@@ -1,3 +1,3 @@
-module github.com/abdularis/test-go-dep-resolve/mod-super
+module github.com/abdularis/test-go-dep-resolve/mod-super/v2
 
 go 1.18
--- mod-super/v2/print.go
+++ mod-super/v2/print.go
@@ -3,5 +3,5 @@ package modsuper
 import "fmt"
 
 func PrintModSuper() {
-	fmt.Println("print from module super (THIS IS VERSION v2.0.2) 🍉 with sub module for v2")
+	fmt.Println("print from module super (THIS IS VERSION v2.0.3) 🍉 with sub module for v2")
 }
